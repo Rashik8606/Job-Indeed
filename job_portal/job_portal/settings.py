a461a8877379eae4d388142c5e081191c6ef5ebf
rashik <mhdrashik112@gmail.com>
2025-05-02 19:52:45 +0530

    Some Changes

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -174,4 +174,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rrashik409@gmail.com'
-EMAIL_HOST_PASSWORD = 'nzcnveckkkibvfhx'  # Use the app password here
+EMAIL_HOST_PASSWORD = 'nzcnveckkkibvfhx' 
